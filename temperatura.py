c4b2290f2d48a2842ffece6fe1774e07b4acc39d
Kanefav <gabrielfav12@gmail.com>
2022-02-03 15:19:41 -0300

    temp 1.0

--- temperatura.py
+++ temperatura.py
@@ -7,11 +7,27 @@ class temp:
     def translate(temp, amount, temp1):
         return ((amount * temp1.num) - (temp.start * temp1.num) + (temp1.start * temp.num)) / temp.num 
                            
+                           
+    def FandC(temp, amount):
+        if temp == 'f': return (amount - 32) / 1.8 # f to c
+        return amount * 1.8 + 32 # c to f
+
+
+    def KandC(temp, amount):
+        if temp == 'k': return amount - 273 # k to c
+        return amount + 273 # c to k
+
+
+    def FandK(temp, amount):
+        pass
+
 
 celsius = temp('c', 0, 5)
 fahrenheit = temp('f', 32, 9)
 kelvin = temp('k', 273, 5)
+x = temp('x', 20, 3)
 while True:
+    break
     ask = str(input('1 - celsius, 2 - fahrenheit, 3 - kelvin: '))
     if ask == '1':
         ask = celsius
@@ -29,3 +45,6 @@ while True:
         ask1 = kelvin
     amount = int(input('temperatura: '))
     print(temp.translate(ask, amount, ask1))    
+#print(temp.translate(celsius, 58.888888888888886, fahrenheit), 'class')
+print(temp.FandC('c', 58), 'notClass')
+print(temp.KandC('k', 298))

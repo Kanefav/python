c46e597ed65e12b429398963630617fc4b15eee7
Kanefav <gabrielfav12@gmail.com>
2022-04-15 12:22:47 -0300

    equation solver

--- equation.py
+++ equation.py
@@ -1,17 +1,28 @@
 from math import sqrt
 
 
+def sinal(x, s): # temp, sina
+    if s == 1:
+        return -int(x)
+    else:
+        return int(x)
+    
+
 def equation(e, g): # equation, grau
     values = [] # a-0 b-1 c-2
     temp = ''
-    
-    for letter in range(0, len(e)):                     # complete the sinals (theyre not being used)
+    s = 0
+    for letter in range(0, len(e)):  
+        if e[letter] == '-':
+            s = 1
         if e[letter] in '1234567890':
             temp += f'{e[letter]}'
             if letter+1 == len(e):
+                temp = int(temp)
                 values.append(temp)
         else:
             if len(temp) > 0:
+                temp = sinal(temp, s)
                 values.append(temp)
             temp = ''
             
@@ -20,11 +31,11 @@ def equation(e, g): # equation, grau
     if delta <= 0:
         raizD = 0
     else:
-        raizD = int(sqrt(delta))
-    bhaskara = -(int(values[1])) + raizD
-    bhaskara2 = -(int(values[1])) - raizD
-    b = bhaskara // (2*int(values[0]))
-    b2 = bhaskara2 // (2*int(values[0]))
+        raizD = sqrt(delta)
+    bhaskara = -(values[1]) + raizD
+    bhaskara2 = -(values[1]) - raizD
+    b = bhaskara // (2*values[0])
+    b2 = bhaskara2 // (2*values[0])
     return [b, b2]
 
         
